b476259585c97e2035cbba93ee1107377d1c17af
nurulalam <jack245200@gmail.com>
2022-10-11 20:50:04 +0600

    corected answer display by state

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
-import { BeakerIcon, EyeIcon } from "@heroicons/react/24/solid";
+import { BeakerIcon, EyeIcon, EyeSlashIcon } from "@heroicons/react/24/solid";
 
 const Question = ({ quiz }) => {
   console.log(quiz);
   const { id, correctAnswer, options, question } = quiz;
-  const [checked, setChecked] = useState(false);
   const [open, setOpen] = useState(false);
 
   return (
@@ -14,15 +13,23 @@ const Question = ({ quiz }) => {
           <p className="text-lg font-semibold">
             Quiz : {question.split("<p>")}
           </p>
-          <EyeIcon onClick={setOpen(!open)} className="h-6 w-6"></EyeIcon>
+          <div className="h-6 w-6" onClick={() => setOpen(!open)}>
+            {open ? <EyeIcon></EyeIcon> : <EyeSlashIcon></EyeSlashIcon>}
+          </div>
         </div>
         <div className="grid grid-cols-2">
           {options.map((option, idx) => (
             <label key={idx}>
-              <input type="checkbox" onChange={() => setChecked(!checked)} />
+              <input type="checkbox" />
               {option}
             </label>
           ))}
+
+          {open === true && (
+            <p className="text-green-600 text-lg font-medium">
+              Correct Answer : {correctAnswer}
+            </p>
+          )}
         </div>
       </div>
     </div>
