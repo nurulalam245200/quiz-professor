e8e27663755ad4c154cb664c365cd7129e6cbed5
nurulalam <jack245200@gmail.com>
2022-10-12 04:10:59 +0600

    statistics site headline and margin complete

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -14,16 +14,20 @@ const Statistics = () => {
   const name = loadedData.name;
   const total = loadedData.total;
   return (
-    <div className="">
-      <h1>Our Topic Status</h1>
-      <ResponsiveContainer width="80%" height={200}>
-        <BarChart width={150} height={40} data={loadedData}>
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Bar dataKey="total" fill="#8884d8" />
-        </BarChart>
-      </ResponsiveContainer>
+    <div>
+      <h1 className="text-4xl font-bold bg-red-500 text-white">
+        Our Topic Status
+      </h1>
+      <div className="mt-8">
+        <ResponsiveContainer width="80%" height={200}>
+          <BarChart width={150} height={40} data={loadedData}>
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Bar dataKey="total" fill="#8884d8" />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 };
