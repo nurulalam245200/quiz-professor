47ab281f71580c2bd3aec807c9a2986e58235c09
nurulalam <jack245200@gmail.com>
2022-10-12 18:27:40 +0600

    padding and complete check

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,12 +7,12 @@ const Home = () => {
       <h1 className="text-white text-4xl font-bold bg-red-500">
         Our Quiz Professor Team Here!
       </h1>
-      <p className="text-white text-lg font-semibold bg-red-500">
+      <p className="text-white text-lg p-8 font-semibold bg-red-500">
         Our platfrom provide a greate full opportiunities for student education.
-        we will provide quiz question and it is about programming world.Most of
-        the country now involed in internet. And web development can easy to
-        create a global communication for world people.And we also want to share
-        our workl with you.
+        we will provide quiz question and it is about programming world. <br />{" "}
+        Most of the country now involed in internet. And web development can
+        easy to create a global communication for world people. <br />
+        And we also want to share our workl with you.
       </p>
       <img className="w-full" src={logo} alt="" />
     </div>
